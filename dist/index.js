4e571f729761a466a37397e0a7335e34b18c106d
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2025-10-02 17:05:17 +0200

    Get the first found file path or null if not found Regenerated dist/index.js

--- dist/index.js
+++ dist/index.js
@@ -123995,7 +123995,9 @@ function searchBuildinfo() {
 function searchFilePath(fileName) {
     const searchPath = `**/${fileName}`;
     core.info(`Search for file in ${searchPath}!`);
-    return globSync(searchPath);
+    const filePaths = globSync(searchPath);
+    // Return the first found file path or null if not found
+    return filePaths.length > 0 ? filePaths[0] : null;
 }
 
 module.exports = {
